af1c2c31ef1ffb6779e3b6e829f15c1c446a538d
e.alexandrova <kk.bpvv@gmail.com>
2024-08-30 14:50:15 +0500

    #1 initial commit, сборка проекта

---
+++ src/app/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-black w-[300px]">
+    Hello
+  </div>
+</template>
+
---
+++ src/app/index.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './styles/tailwind/base.postcss'
+
+import './styles/index.scss'
+
+import App from './App.vue'
+import './styles/tailwind/index.postcss'
+
+export const app
+= createApp(App)
